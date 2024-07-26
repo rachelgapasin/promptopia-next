4ec116fc2aeb68fb8b7b0fef4e2a4341dcb84782
Rachel Gapasin <115187285+rachelgapasin@users.noreply.github.com>
2024-07-26 01:20:44 -0400

    Add features for signing in/out, creating/editing/deleting posts, and viewing own profile

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import Form from "@components/Form";
+
+const EditPrompt = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+
+  const [sumbitting, setSumbitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+
+    if (promptId) getPromptDetails();
+  }, [promptId]);
+
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setSumbitting(true);
+
+    if (!promptId) return alert("Prompt ID not found.");
+
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/profile");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSumbitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      sumbitting={sumbitting}
+      handleSubmit={updatePrompt}
+    />
+  );
+};
+
+export default EditPrompt;
