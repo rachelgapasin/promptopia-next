871e6d537bafa7d3e94bf2aea757316979ff46d0
Rachel Gapasin <115187285+rachelgapasin@users.noreply.github.com>
2024-07-26 19:03:23 -0400

    Make sure profile reloads with current data

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,27 +1,27 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
-  const { data: session } = useSession();
   const router = useRouter();
+  const { data: session } = useSession();
 
-  const [posts, setPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
 
-      setPosts(data);
+      setMyPosts(data);
     };
 
     if (session?.user.id) fetchPosts();
-  }, []);
+  }, [session?.user.id]);
 
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
@@ -38,9 +38,9 @@ const MyProfile = () => {
           method: "DELETE",
         });
 
-        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
 
-        setPosts(filteredPosts);
+        setMyPosts(filteredPosts);
       } catch (error) {
         console.log(error);
       }
@@ -50,8 +50,8 @@ const MyProfile = () => {
   return (
     <Profile
       name="My"
-      desc="Welcome to your personalized profile page!"
-      data={posts}
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination."
+      data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -24,14 +24,14 @@ const Feed = () => {
 
   const handleSearchChange = (e) => {};
 
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setPosts(data);
+  };
+
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch("/api/prompt");
-      const data = await response.json();
-
-      setPosts(data);
-    };
-
     fetchPosts();
   }, []);
 
