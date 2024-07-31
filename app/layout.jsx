616734713b83f11760d3995bf7f8383355060a7c
Rachel Gapasin <115187285+rachelgapasin@users.noreply.github.com>
2024-07-31 16:16:21 -0400

    Make pages dynamic

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,8 @@ import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
+export const dynamic = "force-dynamic";
+
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
