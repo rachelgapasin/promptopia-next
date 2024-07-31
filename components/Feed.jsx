993a7d9959d872ad431e996482255195c8a94cdf
Rachel Gapasin <115187285+rachelgapasin@users.noreply.github.com>
2024-07-31 14:45:08 -0400

    Fix setAllPosts() typo

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -52,7 +52,7 @@ const Feed = () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
 
-      setPosts(data);
+      setAllPosts(data);
     };
 
     fetchPosts();
